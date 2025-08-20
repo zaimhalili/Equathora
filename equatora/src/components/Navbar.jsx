9b7889491f0a4272f35ca987f15d8f5f5c6ff66e
Zaim Halili <halilizaim@gmail.com>
2025-08-20 17:36:46 +0200

    .

--- equatora/src/components/Navbar.jsx
+++ equatora/src/components/Navbar.jsx
@@ -12,8 +12,11 @@ const Navbar = () => {
     <>
       <header id='navbar'>
         <ul id="navbar-left">
-          <li><img src={Logo} alt="Logo" id='navbar-logo' /></li>
-          <li><h3 id='navbar-name'><Link to="/dashboard" className="navbar-pages" id='navbar-dashboard'>equatora</Link></h3></li>
+          <Link to="/dashboard" className="navbar-dashboard" id='navbar-dashboard'>
+            <li><img src={Logo} alt="Logo" id='navbar-logo' /></li>
+            <li><h3 id='navbar-name'>equatora</h3></li>
+          </Link>
+          
           <li>
             <Link to="/learn" className="navbar-pages" id='navbar-learn'>
               Learn
@@ -36,19 +39,19 @@ const Navbar = () => {
         </ul>
         <ul id="navbar-right">
           <li>
-            <Link to="/notifications" className="navbar-notifications" id='navbar-notifications'>
+            <Link to="/notifications" className="navbar-pages" id='navbar-notifications'>
               <img src={Notifications} alt="notifications" id='notificationsIMG' />
             </Link>
           </li>
 
           <li>
-            <Link to="/achievements" className="navbar-achievements" id='navbar-achievements'>
+            <Link to="/achievements" className="navbar-pages" id='navbar-achievements'>
               <img src={Achievements} alt="achievements" id='achievementsIMG' />
             </Link>
           </li>
 
           <li>
-            <Link to="/profile" className="navbar-profile" id='navbar-profile'>
+            <Link to="/profile" className="navbar-pages" id='navbar-profile'>
               <img src={GuestAvatar} alt="avatar" id='avatarIMG' />
             </Link>
           </li>
