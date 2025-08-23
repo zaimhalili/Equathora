2fb5fbd36ea28af96b81c0d23d55db34d242bda5
Zaim Halili <halilizaim@gmail.com>
2025-08-23 11:08:09 +0200

    created the footer and added its children

--- equatora/src/components/Footer.jsx
+++ equatora/src/components/Footer.jsx
@@ -1,14 +1,37 @@
 import React from 'react';
+import './Footer.css';
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
   return (
     <>
-      <footer>
-        <div className="footer-top">
-
-        </div>
-        <div className="footer-bottom">
-          
+      <footer id='footer-container'>
+        <div id="footer-top">
+          <div id="left">
+            <Link to="/learn" className='footer-links'>Learn</Link>
+            <Link to="/discover" className='footer-links'>Discover</Link>
+            <Link to="/achievements" className='footer-links'>Achievements</Link>
+            <Link to="/notifications" className='footer-links'>Notifications</Link>
+            <Link to="/profile" className='footer-links'>My Profile</Link>
+          </div>
+          <div className="center">
+            <h1>
+              Your contribution helps us keep this learning platform available for <span style={{ color:'var(--dark-accent-color)'}}>students worldwide</span>
+            </h1>
+            <div id="socialMedia-container">
+              <a href="#" className='socialMedia'></a>
+              <a href="#" className='socialMedia'></a>
+              <a href="#" className='socialMedia'></a>
+            </div>
+            <h2>Equatora</h2>
+          </div>
+          <div id="right">
+            <Link to="/" className='footer-links'>About Us</Link>
+            <Link to="/" className='footer-links'>Legal & Policies</Link>
+            <Link to="/" className='footer-links'>Cookie Policy</Link>
+            <Link to="/" className='footer-links'>FAQS</Link>
+            <Link to="/" className='footer-links'>Report</Link>
+          </div>
         </div>
       </footer>
     </>
