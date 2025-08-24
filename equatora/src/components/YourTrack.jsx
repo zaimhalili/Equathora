52540e04f45c6425a7749aa1b5be009dc5f94915
Zaim Halili <halilizaim@gmail.com>
2025-08-24 18:00:42 +0200

    fixed progress bar

--- equatora/src/components/YourTrack.jsx
+++ equatora/src/components/YourTrack.jsx
@@ -4,26 +4,32 @@ import QuestionMark from '../assets/images/questionMark.svg';
 import LilArrow from '../assets/images/lilArrow.svg';
 import { Link } from 'react-router-dom';
 
-
 const YourTrack = () => {
+    const solved = 25;
+    const total = 50;
+    const percentage = (solved / total) * 100;
+
     return (
         <>
             <article id='your-track-container'>
                 <div id='progress-bar-track'>
                     <h3>Your Track</h3>
                     <h4>Problems</h4>
-                    <Link to="/problems" id='progressBar-container'>
-                        <progress value={50} max={100} id='progressBar'></progress>
-                    </Link>
                     <div className="prg-bar-arrow">
+                        <Link to="/problems" id='progressBar-container'>
+                            <div
+                                id='measure-progressBar'
+                                style={{ width: `${percentage}%` }} // ✅ Dynamic width
+                            />
+                        </Link>
                         <Link to="/problems" className="arrowProblems" id='arrowProblems'>
                             <img src={LilArrow} alt="arrow" id='arrowProblemsIMG' />
                         </Link>
                     </div>
 
-
                     <div id='noProblemsSolved'>
-                        <span id='completed-problems'>25</span>/<span className='problems-count'>50</span> Problems Solved
+                        <span id='completed-problems'>{solved}</span>/
+                        <span className='problems-count'>{total}</span> Problems Solved
                     </div>
                 </div>
                 <figure>
