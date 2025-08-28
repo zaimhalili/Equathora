a630ba3e4ca299b9a40cab2e49f6e84a215b6de0
Zaim Halili <halilizaim@gmail.com>
2025-08-28 22:28:53 +0200

    Added all the images for the dropdown

--- equatora/src/components/Navbar.jsx
+++ equatora/src/components/Navbar.jsx
@@ -9,7 +9,24 @@ import Sidebar from './Sidebar';
 import Menu from '../assets/images/menu.svg';
 import Dropdown from './Dropdown';
 
-import Premium from '../assets/images/donate.svg';
+import Daily from '../assets/images/questionMark.svg';
+import Leaderboards from '../assets/images/leaderboards.svg';
+import Favourite from '../assets/images/favourite.svg';
+import Premium from '../assets/images/Premium.svg';
+import Progress from '../assets/images/Progress.svg';
+import Choice from '../assets/images/choice.svg';
+import Journey from '../assets/images/journey.svg';
+import Mentoring from '../assets/images/mentoring.svg';
+import Faq from '../assets/images/faq.svg';
+import AboutUs from '../assets/images/about.svg';
+import LogoutIMG from '../assets/images/logout.svg';
+import Statistics from '../assets/images/statistics.svg';
+import Settings from '../assets/images/settings.svg';
+import Updates from '../assets/images/updates.svg';
+import Notifications from '../assets/images/notificationsDD.svg';
+import Teacher from '../assets/images/teacher.svg';
+import Achievements from '../assets/images/achievementsDD.svg';
+import Events from '../assets/images/specialEvents.svg';
 
 const Navbar = () => {
   const [sidebarOpen, setSidebarOpen] = useState(false);
@@ -19,19 +36,19 @@ const Navbar = () => {
       to: "/",
       text: "Daily Problem",
       description: "Solve a fresh daily challenge.",
-      image: GuestAvatar
+      image: Daily
     },
     {
       to: '/learn',
       text: "Your Track",
       description: "Track topics and problems solved.",
-      image: GuestAvatar
+      image: Journey
     },
     {
       to: "/",
-      text: "Saved Problems",
+      text: "Favourite Problems",
       description: "Quickly revisit starred problems.",
-      image: GuestAvatar
+      image: Favourite
     }
   ]
 
@@ -40,19 +57,19 @@ const Navbar = () => {
       to: '/learn',
       text: "Recommended for You",
       description: "Personalized problem suggestions.",
-      image: GuestAvatar
+      image: Choice
     },
     {
       to: '/learn',
       text: "Leaderboards",
       description: "Join competitions and leaderboards.",
-      image: GuestAvatar
+      image: Leaderboards
     },
     {
       to: "/",
       text: "Learning Paths",
       description: "Curated sequences of related problems.",
-      image: GuestAvatar
+      image: Progress
     }
   ]
 
@@ -61,19 +78,19 @@ const Navbar = () => {
       to: '/learn',
       text: "Teacher/Tutor Mode",
       description: "Monitor students or children's progress.",
-      image: GuestAvatar
+      image: Mentoring
     },
     {
       to: '/learn',
       text: "Help Center",
       description: "FAQs and platform support.",
-      image: GuestAvatar
+      image: Faq
     },
     {
       to: "/",
       text: "About Equatora",
-      description: "Learn about mission and vision.",
-      image: GuestAvatar
+      description: "Learn about its mission and vision.",
+      image: AboutUs
     }
   ]
 
@@ -82,19 +99,19 @@ const Navbar = () => {
       to: '/notifications',
       text: "All Notifications",
       description: "View everything at once.",
-      image: GuestAvatar
+      image: Notifications
     },
     {
       to: '/notifications',
       text: "From Teachers/Mentors",
       description: "Messages or assignments from mentors.",
-      image: GuestAvatar
+      image: Teacher
     },
     {
       to: '/notifications',
       text: "System Update",
       description: "Check out our new features",
-      image: GuestAvatar
+      image: Updates
     }
   ];
 
@@ -103,19 +120,19 @@ const Navbar = () => {
       to: '/achievements/recent',
       text: "All Achievements & Badges",
       description: "See all unlocked badges.",
-      image: GuestAvatar
+      image: Achievements
     },
     {
       to: '/achievements/stats',
       text: "Statistics",
       description: "Your learning progress overview",
-      image: GuestAvatar
+      image: Statistics
     },
     {
       to: '/achievements/badges',
       text: "Special Events",
       description: "Seasonal or limited-time achievements.",
-      image: GuestAvatar
+      image: Events
     }
   ];
 
@@ -130,13 +147,13 @@ const Navbar = () => {
       to: '/settings',
       text: "Settings",
       description: "Manage your preferences",
-      image: GuestAvatar
+      image: Settings
     },
     {
       to: '/logout',
       text: "Sign Out",
       description: "Securely log out of your account",
-      image: GuestAvatar
+      image: LogoutIMG
     },
     {
       to: '/logout',
