503161ec1a926fb657e8a6deca13a749ec1155ca
Zaim Halili <halilizaim@gmail.com>
2025-09-02 12:37:04 +0200

    changed the filter ahead of the image and added the links to load other components

--- equathora/src/pages/Achievements/AchievementsLayout.jsx
+++ equathora/src/pages/Achievements/AchievementsLayout.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
+import { Outlet, Link } from 'react-router-dom';
 import Navbar from '../../components/Navbar.jsx';
 import Footer from '../../components/Footer.jsx';
 import './AchievementsLayout.css';
+import AchievementsDD from '../../assets/images/AchievementsDD.svg';
+import Statistics from '../../assets/images/statistics.svg';
+import Events from '../../assets/images/specialEvents.svg';
 
 const AchievementsLayout = () => {
   return (
@@ -9,18 +13,34 @@ const AchievementsLayout = () => {
       <header>
         <Navbar></Navbar>
       </header>
+
       <main className='achievements-body'>
         <div className="achievements-image-body">
           <h1>Your Learning <span id='prg-hover'>Progress</span></h1>
           <h3>Exploring your time on Equathora</h3>
-        </div>
-        <div className="achievements-filter">
+          <div className="achievements-filter">
+            <Link to="recent" className='achievements-link'>
+              <img src={AchievementsDD} alt="achievements" />
+              All Achievements
+            </Link>
 
+            <Link to="stats" className='achievements-link'>
+              <img src={Statistics} alt="statistics" />
+              Statistics
+            </Link>
+
+            <Link to="events" className='achievements-link'>
+              <img src={Events} alt="achievements" />
+              Special Events
+            </Link>
+          </div>
         </div>
         <div className='achievements-content'>
 
         </div>
+
       </main>
+
       <footer>
         <Footer></Footer>
         <a href="http://www.freepik.com" id='freepik-link' target="_blank"
