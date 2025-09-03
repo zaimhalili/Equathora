414011f892c95e9711fe4448804d0ac272c1a703
Zaim Halili <halilizaim@gmail.com>
2025-09-03 17:27:13 +0200

    finished recent achievements layout

--- equathora/src/pages/Achievements/RecentAchievements.jsx
+++ equathora/src/pages/Achievements/RecentAchievements.jsx
@@ -2,20 +2,20 @@ import React from 'react';
 import './AchievementsLayout.css';
 
 const RecentAchievements = () => {
-  const achievements = 5;
+  const achievements = 120;
   return (
     <section className='rec-achievements'>
       <article className="a-top">
         <h2>Your Achievements</h2>
         <p>You have come a long way</p>
         <div className="block-container">
-          <div className="block"><span style={{fontSize: "clamp(3rem, 4vw, 4rem)"}}>{achievements}</span> <br />
+          <div className="block"><span style={{ fontSize: "clamp(3rem, 4vw, 4rem)", fontFamily: "'DynaPuff', serif", color: "white" }}>{51}</span> <br />
             Achievements Earned
           </div>
-          <div className="block"><span style={{ fontSize: "clamp(3rem, 4vw, 4rem)" }}>{achievements}</span> <br />
+          <div className="block"><span style={{ fontSize: "clamp(3rem, 4vw, 4rem)", fontFamily: "'DynaPuff', serif", color: "white" }}>{achievements}</span> <br />
             Days Streak
           </div>
-          <div className="block"><span style={{ fontSize: "clamp(3rem, 4vw, 4rem)" }}>{achievements}</span> <br />
+          <div className="block"><span style={{ fontSize: "clamp(3rem, 4vw, 4rem)", fontFamily: "'DynaPuff', serif", color: "white" }}>{3}</span> <br />
             Concept Learned
           </div>
         </div>
