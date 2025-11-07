ac1375140ce1cb01aa2a1dcade2d17ca5952d122
Zaim Halili <halilizaim@gmail.com>
2025-11-07 23:25:42 +0100

    feat: added some components to settings page

--- equathora/src/pages/Settings.jsx
+++ equathora/src/pages/Settings.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
+import { Link } from 'react-router-dom';
 
 const Settings = () => {
     return (
-        <div></div>
+        <div className=''>
+            <Navbar />
+            <h1>Settings</h1>
+            <Link to="/dashboard">Go to Dashboard</Link>
+            <Footer />
+        </div>
     );
 };
 
