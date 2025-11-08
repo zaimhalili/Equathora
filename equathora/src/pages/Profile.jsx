fc4e073a1ddefb66533fe6bdae0c374a004490d9
Zaim Halili <halilizaim@gmail.com>
2025-11-08 19:21:40 +0100

    feat: add Link import to Profile page for navigation enhancements

--- equathora/src/pages/Profile.jsx
+++ equathora/src/pages/Profile.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 import Autumn from '../assets/images/autumn.jpg';
