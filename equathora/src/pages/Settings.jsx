5f113bfb8eaba89a6bcd4c16f3e816f33ae2f892
Zaim Halili <halilizaim@gmail.com>
2025-11-15 21:41:29 +0100

    style: fixed styling issues in navbar and settings

--- equathora/src/pages/Settings.jsx
+++ equathora/src/pages/Settings.jsx
@@ -16,6 +16,8 @@ const Settings = () => {
             <Navbar />
             <main className='min-h-screen flex flex-col gap-5 xl:gap-8 bg-[linear-gradient(180deg,var(--mid-main-secondary),var(--main-color)50%)] items-center text-[var(--secondary-color)] px-8 sm:px-12 md:px-20 lg:px-30 xl:px-72'>
                 <h1 className='font-[Public Sans,cursive] font-bold text-[var(--secondary-color)] text-2xl lg:text-3xl pt-5'>Settings & Customization</h1>
+
+                {/* Profile Section */}
                 <section className='bg-white shadow-gray-500 shadow-2xl rounded-xl w-full px-5 pt-3 pb-5 max-w-[800px]'>
                     <h2 className='font-bold text-xl lg:text-3xl text-[var(--secondary-color)] pb-3'>Profile</h2>
                     <div>
@@ -55,7 +57,7 @@ const Settings = () => {
                                 id=""
                                 value={userBio}
                                 onChange={(e) => setUserBio(e.target.value)}
-                                className='text-md xl:text-lg border rounded-lg px-3 py-2 lg:py-4 w-full border-[var(--french-gray)] focus:shadow-[var(--accent-color)] h-24 lg:h-32 resize-none scrollbar-thin scrollbar-thumb-gray-400 scrollbar-track-gray-100'
+                                className='text-md xl:text-lg border rounded-lg px-3 py-2 w-full border-[var(--french-gray)] focus:shadow-[var(--accent-color)] h-24 lg:h-32 resize-none scrollbar-thin scrollbar-thumb-gray-400 scrollbar-track-gray-100'
                                 placeholder="Tell the world about yourself..."
                             ></textarea>
                             <p className='text-xs lg:text-md'>Tell the world about you! Be proud!</p>
@@ -87,6 +89,8 @@ const Settings = () => {
                     <hr className='pb-3' />
                     <button type="submit" className='cursor-pointer py-2 px-3 bg-[var(--accent-color)] text-white font-bold text-md rounded-lg shadow-md shadow-gray-400 border-1 border-black hover:bg-[var(--dark-accent-color)] transition-all duration-150 max-w-[180px]'>Save profile data</button>
                 </section>
+
+                {/* Handle Section */}
                 <section className='bg-white shadow-gray-500 shadow-2xl rounded-xl w-full px-5 pt-3 pb-5 flex flex-col gap-2 lg:gap-4 max-w-[800px]'>
                     <h1 className='font-[Public Sans,cursive] font-bold text-[var(--secondary-color)] text-2xl pt-5 lg:text-3xl'>Change your handle</h1>
                     <div>
@@ -114,7 +118,7 @@ const Settings = () => {
                         <h4 className='font-[Public Sans,cursive] font-bold text-[var(--secondary-color)] text-lg lg:text-2xl'>Reset Account</h4>
                         <p className='text-md'>Resetting your account will reset your progress on all tracks,
                             reset all exercises and remove access to all your previous mentoring.</p>
-                        <button type="submit" className='cursor-pointer py-2 px-3 bg-[var(--accent-color)] text-white font-bold text-md border-1 border-black hover:bg-[var(--dark-accent-color)] transition-all duration-150 lg:max-w-[180px]'>Reset account</button>
+                        <button type="submit" className='cursor-pointer py-2 px-3 bg-[var(--accent-color)] text-white font-bold text-md border-1 border-black hover:bg-[var(--dark-accent-color)] transition-all rounded-xs duration-200 lg:max-w-[180px] hover:-translate-y-1'>Reset account</button>
                     </div>
 
                     <div className='w-full border-2 border-[var(--dark-accent-color)] rounded-md lg:w-1/2
@@ -122,7 +126,7 @@ const Settings = () => {
                         <h4 className='font-[Public Sans,cursive] font-bold text-[var(--secondary-color)] text-lg lg:text-2xl'>Delete Account</h4>
                         <p className='text-md'>This option will eliminate your account entirely with all data.
                             You’ll get a chance to confirm your choice.</p>
-                        <button type="submit" className='cursor-pointer py-2 px-3 bg-[var(--accent-color)] text-white font-bold text-md border-1 border-black hover:bg-[var(--dark-accent-color)] transition-all duration-150 lg:max-w-[180px]'>Delete account</button>
+                        <button type="submit" className='cursor-pointer py-2 px-3 rounded-xs bg-[var(--accent-color)] text-white font-bold text-md border-1 border-black hover:bg-[var(--dark-accent-color)] transition-all duration-200 hover:-translate-y-1 lg:max-w-[180px]'>Delete account</button>
                     </div>
                 </section>
             </main>
