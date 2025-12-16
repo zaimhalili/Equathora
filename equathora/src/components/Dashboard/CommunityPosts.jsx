c1f179555d0354bb009ee047f703d7c63dfc031c
Zaim Halili <halilizaim@gmail.com>
2025-12-16 18:47:30 +0100

    style: minor style changes

--- equathora/src/components/Dashboard/CommunityPosts.jsx
+++ equathora/src/components/Dashboard/CommunityPosts.jsx
@@ -14,14 +14,14 @@ const CommunityPosts = () => {
             <h3 className="text-[var(--secondary-color)] font-[Inter] text-2xl font-bold text-center md:text-left">
               Announcements & Discussions
             </h3>
-            <div className='flex gap-3 flex-wrap'>
+            <div className='flex gap-3 flex-wrap justify-center md:justify-start'>
               {/* <Link to="" className='bg-white rounded-md shadow-[0_10px_10px_rgba(141,153,174,0.3)] w-1/3 py-3 md:py-5 px-6 flex gap-3 flex-col hover:shadow-[0_0_25px_rgba(141,153,174,0.7)] transition-all cursor-pointer hover:scale-105 duration-200 ease-out min-w-50 flex-1'>
                 <img src={Autumn} alt="" className='rounded-md max-h-45 md:h-1/2' />
                 <p className='text-md text-left lg:text-lg font-[Inter] text-[var(--secondary-color)] font-bold cursor-pointer'>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
                 <p className='text-[var(--mid-main-secondary)] font-[Inter]'>Zaim ⋅ 1 week ago</p>
               </Link> */}
 
-              <Link to="/waitlist" className='bg-white rounded-md shadow-[0_10px_10px_rgba(141,153,174,0.3)] w-1/3 py-5 px-6 flex gap-3 flex-col hover:shadow-[0_0_25px_rgba(141,153,174,0.7)] transition-all duration-200 ease-out hover:scale-105 cursor-pointer min-w-50'>
+              <Link to="/waitlist" className='bg-white rounded-md shadow-[0_10px_10px_rgba(141,153,174,0.3)] w-1/3 py-5 px-6 flex gap-3 flex-col hover:shadow-[0_0_25px_rgba(141,153,174,0.7)] transition-all duration-200 ease-out hover:scale-105 cursor-pointer min-w-50 flex-1 max-w-80'>
                 <img src={Journey} alt="" className='rounded-md max-h-45 md:h-1/2' />
                 <p className='text-md text-left lg:text-lg cursor-pointer font-[Inter] text-[var(--secondary-color)] font-bold'>Join our waitlist to recieve weekly updates</p>
                 <p className='text-[var(--mid-main-secondary)] font-[Inter]'>Zaim ⋅ 1 day ago</p>
