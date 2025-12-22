5547af7dc2a4478576eb97a4e8e404aa804940af
Zaim Halili <halilizaim@gmail.com>
2025-12-22 19:18:10 +0100

    feat: added missing library and created two classes: User and DbContext

---
+++ equathora/backend/EquathoraBackend/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using EquathoraBackend.Models;
+
+namespace EquathoraBackend.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; } = null!;
+    }
+}
