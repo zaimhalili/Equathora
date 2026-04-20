2bad94095d4d1cd67d77d09f9eca16b48f715826
Zaim Halili <halilizaim@gmail.com>
2026-04-20 14:06:25 +0200

    fix: update CTASection button text color for better visibility and comment out EquathoraBriefsSection in Landing page

--- equathora/src/pages/Landing.jsx
+++ equathora/src/pages/Landing.jsx
@@ -44,7 +44,7 @@ const Landing = () => {
                 <LatestArticlesSection />
                 <hr class="border-[var(--french-gray)]" />
                 <CTASection />
-                <EquathoraBriefsSection />
+                {/* <EquathoraBriefsSection /> */}
                 <Footer />
                 <div className="flex flex-col lg:flex-row items-center text-[var(--french-gray)] bg-[var(--secondary-color)] underline border-t-1 border-gray-50/20 font-light gap-1 text-[12px] py-3 justify-center font-[Sansation] theme-lock">
                     <a href="https://www.vecteezy.com/free-png/student" target="_blank" rel="noopener noreferrer" id="#freepik-link">
