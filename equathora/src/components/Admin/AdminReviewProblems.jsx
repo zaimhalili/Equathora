5f4e98cb8314525db3a1c72f2eaed5aa9a29c096
zaimhalili <halilizaim@gmail.com>
2026-04-24 09:48:16 +0200

    feat: added a TODO

--- equathora/src/components/Admin/AdminReviewProblems.jsx
+++ equathora/src/components/Admin/AdminReviewProblems.jsx
@@ -3,7 +3,12 @@ import React from 'react'
 const AdminReviewProblems = () => {
     return (
         <div>
-
+            {/* TODO: show list of possible new problems */}
+            {/* Add visual elements to understand if it has been published yet
+                if it has already been published it gets a bit hidden but can be still reviewed etc */}
+            {/* A button to copy all of them at once
+            When you click one of them it opens and shows all the other details
+            Other functionalities of your liking */}
         </div>
     )
 }
