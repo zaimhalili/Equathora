17c61f5ac2cada6d1584eed7d0d112a6f2071ff5
Zaim Halili <halilizaim@gmail.com>
2026-05-22 20:56:14 +0200

    feat: add attribution for Worker illustrations in EquathoraBriefsPage

--- equathora/src/pages/EquathoraBriefs.jsx
+++ equathora/src/pages/EquathoraBriefs.jsx
@@ -219,6 +219,11 @@ const EquathoraBriefsPage = () => {
                     </section>
 
                     <Footer />
+                    <div className='w-full bg-[var(--secondary-color)] border-t border-white/10 flex justify-center py-5 text-white/60 text-xs theme-lock'>
+                        <a href="https://storyset.com/worker" target="_blank" rel="noopener noreferrer" className='hover:text-white/80 transition-colors no-underline'>
+                            Worker illustrations by Storyset
+                        </a>
+                    </div>
                 </main>
             </div>
             
